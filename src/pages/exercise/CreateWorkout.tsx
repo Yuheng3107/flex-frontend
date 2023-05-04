3b7dfe26f64bff69951097b797e982105dfc4f28
AppleJem <98686428+AppleJem@users.noreply.github.com>
2023-05-04 16:34:22 +0800

    slight updates on CreateWorkout.tsx

--- src/pages/exercise/CreateWorkout.tsx
+++ src/pages/exercise/CreateWorkout.tsx
@@ -1,5 +1,5 @@
 
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 
 //utils
 import { createUserPostAsync } from '../../utils/data/postData';
@@ -7,8 +7,12 @@ import { createExerciseRegimAsync, getAllExercisesAsync } from '../../utils/data
 
 //types
 import { emptyExerciseData } from '../../types/stateTypes';
+
+//custom hook
+import { useFormInput } from '../../customHooks/useFormInput';
+
 //ionic
-import { add } from 'ionicons/icons';
+import { add, checkmarkCircleOutline, checkmarkOutline } from 'ionicons/icons';
 import {
     IonPage,
     IonContent,
@@ -23,13 +27,21 @@ import {
     IonLabel,
     IonInput,
     IonSelect,
-    IonSelectOption
+    IonSelectOption,
+    IonTitle,
+    IonFab,
+    IonFabButton
 } from '@ionic/react';
 import { exercises } from '../../App';
 
 function CreateWorkout() {
     const [allExercises, setAllExercises] = useState([emptyExerciseData]);
     const [numberOfExercises, setNumberOfExercises] = useState(1);
+    const [exercisesArr, setExercisesArr] = useState<number[]>([]);
+
+    const [nameInputProps, nameInputValue] = useFormInput();
+    const [descriptionInputProps, descriptionInputValue] = useFormInput();
+    const mediaInputRef = useRef<HTMLInputElement>(null);
 
     useEffect(() => {
         async function fetchAllExercisesInfo() {
@@ -42,12 +54,21 @@ function CreateWorkout() {
 
     let exerciseSelects = [];
     for (let i = 0; i < numberOfExercises; i++) {
-        exerciseSelects.push(<IonItem>
-            <IonLabel position="stacked">Exercise {i+1}</IonLabel>
-            <IonSelect interface="popover" placeholder="Exercise Exercise">
+        exerciseSelects.push(<IonItem key={i} >
+            <IonLabel position="stacked">Exercise {i + 1} <button>help</button></IonLabel>
+            <IonSelect interface="popover" placeholder="Exercise Exercise"
+            onIonChange={(e)=> {
+                setExercisesArr(prev=>{
+                    let arr = [...prev];
+                    arr[i] = e.target.value;
+                    console.log(arr);
+                    return arr;
+                })
+            }}>
                 {allExercises.map(item => {
-                    return <IonSelectOption value={item.id}>{item.name}</IonSelectOption>
+                    return <IonSelectOption key={item.id} value={item.id}>{item.name}</IonSelectOption>
                 })}
+                {/* <IonSelectOption key={-1} value={-1}>Remove</IonSelectOption> */}
             </IonSelect>
         </IonItem>);
     }
@@ -55,31 +76,66 @@ function CreateWorkout() {
     function addExerciseHandler() {
         setNumberOfExercises(prev => prev + 1);
     }
+
+    function submitHandler() {
+        let errorFields = []
+        if (nameInputValue.trim() === "") {
+            errorFields.push("name");
+        }
+        if (descriptionInputValue.trim() === "") {
+            errorFields.push("description")
+        }
+        if (mediaInputRef.current!.files !== null) {
+            if (mediaInputRef.current!.files.length <= 0) {
+                errorFields.push("image")
+            }
+        }
+        console.log(errorFields);
+    }
+
+    function nameChangeHandler() {
+
+    }
     return <IonPage>
         <IonHeader>
             <IonToolbar>
                 <IonButtons slot="start">
                     <IonBackButton defaultHref="/exercise">back</IonBackButton>
                 </IonButtons>
+                <IonTitle>
+                    Create Workout
+                </IonTitle>
             </IonToolbar>
         </IonHeader>
         <IonContent>
-            <main>
-                <IonList>
-                    <IonItem>
-                        <IonLabel position="stacked">Name</IonLabel>
-                        <IonInput placeholder="Enter Workout Name"></IonInput>
-                    </IonItem>
-                    <IonItem>
-                        <IonLabel position="stacked">Description</IonLabel>
-                        <IonInput placeholder="Enter Description"></IonInput>
-                    </IonItem>
-                    {exerciseSelects.map(item => item)}
-                    <IonButton onClick={addExerciseHandler} fill="clear">
-                        <IonIcon slot="icon-only" icon={add}></IonIcon>
-                    </IonButton>
-                </IonList>
+            <main className="pr-4">
+                <form id="create-regime-form">
+                    <IonList>
+                        <IonItem>
+                            <IonLabel position="stacked">Name</IonLabel>
+                            {/* <IonInput onChange={nameChangeHandler} placeholder="Enter Workout Name"></IonInput> */}
+                            <input className="bg-transparent focus:outline-none w-full" type="text" placeholder="Enter Name" {...nameInputProps} />
+                        </IonItem>
+                        <IonItem>
+                            <IonLabel position="stacked">Description</IonLabel>
+                            <input className="bg-transparent focus:outline-none w-full" type="text" placeholder="Enter Name" {...descriptionInputProps} />
+                        </IonItem>
+                        <IonItem>
+                            <IonLabel position="stacked">Banner Image</IonLabel>
+                            <input type="file" ref={mediaInputRef} onChange={(e) => console.log(mediaInputRef.current?.files)} />
+                        </IonItem>
+                        {exerciseSelects.map(item => item)}
+                        <IonButton disabled={numberOfExercises >= 30 ? true : false} onClick={addExerciseHandler} fill="clear">
+                            <IonIcon slot="icon-only" icon={add}></IonIcon>
+                        </IonButton>
+                    </IonList>
+                </form>
             </main>
+            <IonFab slot="fixed" vertical="bottom" horizontal="end">
+                <IonFabButton onClick={submitHandler}>
+                    <IonIcon icon={checkmarkOutline}></IonIcon>
+                </IonFabButton>
+            </IonFab>
         </IonContent>
     </IonPage>
 }
