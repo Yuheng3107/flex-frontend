fd14e7f3bababcd902291e1ab90a4b4494729f4e
Kuang Yu Heng <yuheng3107@gmail.com>
2023-05-23 22:17:34 +0800

    Added Inter fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,17 @@
 // tailwind.config.js
 module.exports = {
-  mode: 'jit', // Optionally use just in time engine
-  purge: ['./src/**/*.{js,jsx,ts,tsx,css}', './public/index.html'],
+  mode: "jit", // Optionally use just in time engine
+  purge: ["./src/**/*.{js,jsx,ts,tsx,css}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        inter: ["Inter"],
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
