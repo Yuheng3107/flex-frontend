e0361a7961abe4b86aeeba67e69568511b1fc891
Kuang Yu Heng <yuheng3107@gmail.com>
2023-05-23 11:46:24 +0800

    Positioned settings cog properly

--- src/pages/profile/Profile.tsx
+++ src/pages/profile/Profile.tsx
@@ -2,14 +2,21 @@ import React, { useState, useEffect, FormEvent } from "react";
 
 //redux imports
 import { useAppSelector, useAppDispatch } from "../../store/hooks";
-import { profileDataActions } from '../../store/profileDataSlice';
+import { profileDataActions } from "../../store/profileDataSlice";
 
 //utils imports
-import { getAllPostData, getLikesAsync, getUserPostsAsync } from "../../utils/data/postData";
+import {
+  getAllPostData,
+  getLikesAsync,
+  getUserPostsAsync,
+} from "../../utils/data/postData";
 
 import { googleLogout } from "@react-oauth/google";
-import { PostArray, emptyPostArray, emptyProfileData } from "../../types/stateTypes";
-
+import {
+  PostArray,
+  emptyPostArray,
+  emptyProfileData,
+} from "../../types/stateTypes";
 
 //ionic imports
 import {
@@ -27,15 +34,15 @@ import UserProfileTemplate from "../../components/profile/UserProfileTemplate";
 type ProfileProps = {
   // updateProfileState: number;
   // setUpdateProfileState: (arg: number) => void;
-}
+};
 
-const Tab3 = ({ }: ProfileProps) => {
-  const [posts, setPosts] = useState<PostArray>(emptyPostArray);;
+const Tab3 = ({}: ProfileProps) => {
+  const [posts, setPosts] = useState<PostArray>(emptyPostArray);
   const [currentUserPostSet, setCurrentUserPostSet] = useState(0);
   const dispatch = useAppDispatch();
 
-  const profileDataRedux = useAppSelector((state) => state.profile.profileData)
-  const exerciseStatsRedux = useAppSelector((state) => state.exerciseStats)
+  const profileDataRedux = useAppSelector((state) => state.profile.profileData);
+  const exerciseStatsRedux = useAppSelector((state) => state.exerciseStats);
   console.log(profileDataRedux);
 
   useEffect(() => {
@@ -43,22 +50,37 @@ const Tab3 = ({ }: ProfileProps) => {
   }, [exerciseStatsRedux, profileDataRedux]);
 
   const loadUserPostData = async () => {
-    let postArray = await getUserPostsAsync(profileDataRedux.id, currentUserPostSet);
-    
+    let postArray = await getUserPostsAsync(
+      profileDataRedux.id,
+      currentUserPostSet
+    );
+
     console.log(`set:${currentUserPostSet}`);
     console.log(postArray);
 
-    setPosts(await getAllPostData('user', postArray, posts, profileDataRedux, null))
+    setPosts(
+      await getAllPostData("user", postArray, posts, profileDataRedux, null)
+    );
     setCurrentUserPostSet(currentUserPostSet + 1);
   };
 
   return (
     <IonPage>
       <IonContent fullscreen>
-        <IonButton routerLink="/profile/settings" fill="default" shape="round">
+        <IonButton
+          routerLink="/profile/settings"
+          fill="default"
+          shape="round"
+          className="ion-no-padding mx-2"
+        >
           <IonIcon icon={cog} />
         </IonButton>
-          <UserProfileTemplate profileData={profileDataRedux} exerciseStats={exerciseStatsRedux} posts={posts} loadUserPostData={loadUserPostData} />
+        <UserProfileTemplate
+          profileData={profileDataRedux}
+          exerciseStats={exerciseStatsRedux}
+          posts={posts}
+          loadUserPostData={loadUserPostData}
+        />
       </IonContent>
     </IonPage>
   );
