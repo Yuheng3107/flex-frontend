ce1beb3a45110a05eb9b8f9a45735faa9bacb609
Kuang Yu Heng <yuheng3107@gmail.com>
2023-05-26 15:36:01 +0800

    Added comment

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import { backend } from "./App";
-
+import LikeIcon from "./assets/svgComponents/LikeIcon";
+// App doesn't work for now because of lack of Provider component, some redux issue
 test("check that backend is localhost for dev", () => {
   expect(backend).toBe("http://localhost:8000");
 });
