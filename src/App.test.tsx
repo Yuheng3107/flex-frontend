1053aec9c8e632348e6a7a4744eab1534db170ec
Kuang Yu Heng <yuheng3107@gmail.com>
2023-05-26 15:47:21 +0800

    Added comments

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import { backend } from "./App";
-import LikeIcon from "./assets/svgComponents/LikeIcon";
+import App from "./App";
+import { Provider } from "react-redux";
+
 // App doesn't work for now because of lack of Provider component, some redux issue
+// see https://stackoverflow.com/questions/60329421/usedispatch-error-could-not-find-react-redux-context-value-please-ensure-the
 test("check that backend is localhost for dev", () => {
   expect(backend).toBe("http://localhost:8000");
 });
