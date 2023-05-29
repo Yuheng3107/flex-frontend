045a05466e36fea3bf3b0fed1820a5ae5bdd3aab
AppleJem <98686428+AppleJem@users.noreply.github.com>
2023-05-29 17:56:45 +0800

    better styling search bar

--- src/components/Feed/SearchBar.tsx
+++ src/components/Feed/SearchBar.tsx
@@ -24,9 +24,9 @@ function SearchBar({submitForm, placeholder}: SearchBarProps) {
 
     return <form onSubmit={searchHandler} className="flex flex-row bg-gray-200 dark:bg-zinc-700 rounded-full px-4 py-1 justify-between mx-5">
         <input value={searchInput} onChange={inputChangeHandler} type="text" placeholder={placeholder}
-        className="focus:outline-0 bg-transparent"></input>
+        className="focus:outline-0 bg-transparent dark:text-gray-200"></input>
         <button type="submit">
-            <SearchIcon className="w-6 aspect-square" />
+            <SearchIcon className="w-6 aspect-square dark:stroke-gray-400" />
         </button>
     </form>
 }
